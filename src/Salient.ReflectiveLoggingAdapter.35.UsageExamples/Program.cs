b93cfa3a059002b2ad36f78ee99e066fcccd60d6
bitpusher <sky.sanders@gmail.com>
2012-02-12 15:08:56 -0700

    first commit

---
+++ src/Salient.ReflectiveLoggingAdapter.35.UsageExamples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Salient.ReflectiveLoggingAdapter._35.UsageExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
